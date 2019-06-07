b3ad2496ee665228451afe8f7d1187795bd4d386
Fernando Mendoza R <fernandoza@gmail.com>
2019-06-07 16:19:28 -0500

    refactor: better comments and var names

--- src/DesktopMusicApp.java
+++ src/DesktopMusicApp.java
@@ -7,32 +7,37 @@ import java.util.List;
 public class DesktopMusicApp extends MusicApp implements Playable {
 
     private HashMap<Date, Integer> log;
-    private Date now = new Date(System.currentTimeMillis());
+    private Date today = new Date(System.currentTimeMillis());
 
     public DesktopMusicApp(double version, List<Song> playlist) {
         super(version, playlist);
+        // Initializes the HashMap
         this.log = new HashMap<>();
-        this.log.put(now, 0);
+        this.log.put(today, 0);
     }
 
+    // OOP implemented
     @Override
     public String play(Song song) {
-//        1 display the song info
-        this.log.put(now, this.log.get(now)+1);
+        // 1. display the song info
+        this.log.put(today, this.log.get(today)+1);
 
 //        this.sing(song);
 
-        // 2 Updating the song info UI
+        // 2. Updating the song info UI
         System.out.println("Title = " + song.getName());
 
+        // 3. return url to stream
         return "//streamer/songs/" + super.normalize(song.getName()) + ".mp3";
     }
 
+    // Exposure to Duration class and time handling
     @Override
     public void pause(Song song) {
         // pauses the song
         System.out.println(song.getName() +" was paused at = " + song.getLength().toMinutesPart()+":"+ song.getLength().toSecondsPart());
     }
+
 
     @Override
     public void stop() {
@@ -41,6 +46,7 @@ public class DesktopMusicApp extends MusicApp implements Playable {
         this.printLog();
     }
 
+    // Just for fun, sings the lyrics
     public void sing(Song song){
         try {
             Process process = Runtime.getRuntime().exec(
@@ -50,6 +56,7 @@ public class DesktopMusicApp extends MusicApp implements Playable {
         }
     }
 
+    // How to loop through a HashMap
     public void printLog(){
         for (Date entry: this.log.keySet()) {
             System.out.println(entry + " " + this.log.get(entry));
