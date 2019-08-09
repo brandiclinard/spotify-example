0a4a7e7a7d73b52a6ea62131472eeaa6fc52babb
Brandi <brandiclinard021911@gmail.com>
2019-08-09 09:59:29 -0500

    1st inheritance. desktop class/ implements construtor via extention in desktop by music app

--- src/MusicApp.java
+++ src/MusicApp.java
@@ -7,7 +7,7 @@ public class MusicApp {
     ArrayList<Song> library;
 
 
-    public MusicApp(double version, ArrayList<Song> library) {
+    public MusicApp(double version) {
         this.version = version;
         this.library = LibraryService.updateLibrary();
     }
